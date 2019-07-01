2dccb9832bf946fb8b96db31c012b3527aea1198
KaizN <elvis97rus@gmail.com>
2019-07-01 15:47:08 +0300

    php1-hw7
    Login/authorisation page

---
+++ php/php1/hw/7/login.php
@@ -0,0 +1,15 @@
+<?php
+
+include 'engine/autoload.php';
+autoload('config');
+
+include ENGINE_DIR . 'login.php';
+
+include PUBLIC_DIR . 'header.php';
+
+include TEMPLATES_DIR . 'login.php';
+
+
+
+
+include PUBLIC_DIR . 'footer.php';
---
+++ php/php1/hw/7/templates/catalog.php
@@ -0,0 +1,18 @@
+<div class="container">
+    <?php foreach ($goods as $good): ?>
+       <div class="single-product">
+            <img src="<?= $good['img_address'] ?>" alt="<?= $good['good_name'] ?>">
+           <p><?= $good['good_name'] ?></p>
+           <p><?= $good['good_description'] ?></p>
+           <p>Цена: <?= $good['good_price'] ?>р.</p>
+           <?php if ($good['is_active']==1):?>
+               <p>Товар есть на складе</p>
+           <?php else:?>
+               <p>Ожидаем поступления</p>
+           <?php endif ?>
+           <form action = "/single-product.php" method = "POST">
+               <button type="submit" name="id_good" value="<?= $good['id_good'] ?>" class="mb-3 btn btn-primary">Купить</button>
+           </form>
+       </div>
+    <?php endforeach ?>
+</div>
